3f04e306467da72c216af563f6ff87aac9c337d0
Ahmedlidrissi <lidrissiahmed461@gmail.com>
2026-06-02 10:12:25 +0100

    port update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 // Route imports
 const authRoutes = require('./routes/auth');
